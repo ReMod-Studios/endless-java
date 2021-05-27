c848691cd30bc427e0a34c22db375523f05686f1
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-05-27 15:00:28 +0300

    worldgen :b:roke
    Features are suddenly not registered??
    And I can't figure out Architectury's "server starting" events

--- common/src/main/kotlin/com/remodstudios/endless/world/gen/feature/ConfiguredFeatureRegistry.kt
+++ common/src/main/kotlin/com/remodstudios/endless/world/gen/feature/ConfiguredFeatureRegistry.kt
@@ -1,38 +1,93 @@
 package com.remodstudios.endless.world.gen.feature
 
+import com.mojang.serialization.Lifecycle
 import com.remodstudios.endless.Endless.id
 import com.remodstudios.endless.blocks.BlockRegistry
+import me.shedaniel.architectury.event.events.LifecycleEvent
 import me.shedaniel.architectury.registry.BiomeModifications
 import net.minecraft.block.Blocks
-import net.minecraft.structure.rule.BlockMatchRuleTest
 import net.minecraft.util.registry.BuiltinRegistries
+import net.minecraft.util.registry.DynamicRegistryManager
+import net.minecraft.util.registry.MutableRegistry
+import net.minecraft.util.registry.Registry.CONFIGURED_FEATURE_WORLDGEN
+import net.minecraft.util.registry.RegistryKey
 import net.minecraft.world.biome.Biome
 import net.minecraft.world.gen.GenerationStep
+import net.minecraft.world.gen.UniformIntDistribution
 import net.minecraft.world.gen.feature.ConfiguredFeature
 import net.minecraft.world.gen.feature.ConfiguredFeatures
+import net.minecraft.world.gen.feature.DiskFeatureConfig
 import net.minecraft.world.gen.feature.Feature
-import net.minecraft.world.gen.feature.OreFeatureConfig
+import java.util.Collections.singletonList
+import java.util.function.Supplier
 
 object ConfiguredFeatureRegistry {
-    private val RULE_END_STONE = BlockMatchRuleTest(Blocks.END_STONE)
+    private val END_STONE_LIST = singletonList(Blocks.END_STONE.defaultState)
     private val HEIGHTMAP_SURFACE_SQUARE = ConfiguredFeatures.Decorators.HEIGHTMAP_WORLD_SURFACE.spreadHorizontally()
 
-    private fun register(path: String, feature: ConfiguredFeature<*, *>): ConfiguredFeature<*, *> {
-        return BuiltinRegistries.add(BuiltinRegistries.CONFIGURED_FEATURE, id(path), feature)
+    data class Registration(val key: RegistryKey<ConfiguredFeature<*, *>>, val feature: ConfiguredFeature<*, *>)
+    private val REGISTRATION_LIST = ArrayList<Registration>()
+
+    private fun add(path: String, featureSupplier: Supplier<ConfiguredFeature<*, *>>): ConfiguredFeature<*, *> {
+        val feature = featureSupplier.get()
+        REGISTRATION_LIST.add(Registration(RegistryKey.of(CONFIGURED_FEATURE_WORLDGEN, id(path)), feature))
+        return feature
     }
 
-    private val LUNARIC_END_STONE = register("lunaric_end_stone",
-        Feature.ORE.configure(OreFeatureConfig(RULE_END_STONE, BlockRegistry.LUNARIC_END_STONE.get().defaultState, 9))
-            .decorate(HEIGHTMAP_SURFACE_SQUARE.applyChance(10).repeat(10)))
-    private val COBALT_ORE = register("cobalt_ore",
-        Feature.ORE.configure(OreFeatureConfig(RULE_END_STONE, BlockRegistry.COBALT_ORE.get().defaultState, 6))
-            .decorate(HEIGHTMAP_SURFACE_SQUARE.applyChance(20).repeat(20)))
-    private val THERAKIUM_ORE = register("therakium_ore",
-        Feature.ORE.configure(OreFeatureConfig(RULE_END_STONE, BlockRegistry.THERAKIUM_ORE.get().defaultState, 4))
-            .decorate(HEIGHTMAP_SURFACE_SQUARE.applyChance(40).repeat(10)))
+    private val COBALT_ORE = add("cobalt_ore")
+    {
+        Feature.DISK.configure(
+            DiskFeatureConfig(
+                BlockRegistry.COBALT_ORE.get().defaultState,
+                UniformIntDistribution.of(5, 2), 3, END_STONE_LIST
+            )
+        )
+            .decorate(HEIGHTMAP_SURFACE_SQUARE)
+    }
+    private val THERAKIUM_ORE = add("therakium_ore") {
+        Feature.DISK.configure(
+            DiskFeatureConfig(
+                BlockRegistry.THERAKIUM_ORE.get().defaultState,
+                UniformIntDistribution.of(3, 2), 2, END_STONE_LIST
+            )
+        )
+            .decorate(HEIGHTMAP_SURFACE_SQUARE)
+    }
+    private val LUNARIC_END_STONE = add("lunaric_end_stone") {
+        Feature.DISK.configure(
+            DiskFeatureConfig(
+                BlockRegistry.LUNARIC_END_STONE.get().defaultState,
+                UniformIntDistribution.of(4, 3), 1, END_STONE_LIST
+            )
+        )
+            .decorate(HEIGHTMAP_SURFACE_SQUARE)
+    }
+
+    private fun register(registry: MutableRegistry<ConfiguredFeature<*, *>>) {
+        REGISTRATION_LIST.forEach {
+            if (!registry.containsId(it.key.value))
+                registry.add(it.key, it.feature, Lifecycle.stable())
+        }
+    }
+
+    private fun registerBuiltin() {
+        register(BuiltinRegistries.CONFIGURED_FEATURE as MutableRegistry<ConfiguredFeature<*, *>>)
+    }
+
+    private fun registerDynamic(manager: DynamicRegistryManager) {
+        register(manager.get(CONFIGURED_FEATURE_WORLDGEN))
+    }
 
     fun register() {
         /* clinit */
+        registerBuiltin()
+
+        LifecycleEvent.SERVER_STARTING.register(LifecycleEvent.ServerState { server ->
+            with (server) {
+                println("yo. registering into $this's DynamicRegistryManager")
+                registerDynamic(registryManager)
+            }
+        })
 
         BiomeModifications.addProperties({ bCtx -> bCtx.properties.category == Biome.Category.THEEND })
         { bCtx, bProps ->
